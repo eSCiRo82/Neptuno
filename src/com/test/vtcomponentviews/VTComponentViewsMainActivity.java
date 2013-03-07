bc47e420f825ad8406128fdeb1e53f101901d748
Jose <josealbertofp82@gmail.com>
2013-03-07 12:08:45 +0100

    VTComponentViews - Añadido al repositorio

---
+++ src/com/test/vtcomponentviews/VTComponentViewsMainActivity.java
@@ -0,0 +1,22 @@
+package com.test.vtcomponentviews;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class VTComponentViewsMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_vtcomponent_views_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_vtcomponent_views_main, menu);
+		return true;
+	}
+
+}
